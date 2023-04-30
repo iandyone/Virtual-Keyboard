79b6684b8ede98cf2a17231241b30907b0823bcd
Andrei Bernatski <iandy1731@gmail.com>
2023-04-30 13:08:40 +0300

    feat add character printing on the display

---
+++ src/js/modules/keyboard.js
@@ -0,0 +1,44 @@
+const options = ["AltLeft", "AltRight", "Backspace", "CapsLock", "ControlLeft", "ControlRight", "Delete", "Enter", "ShiftLeft", "ShiftRight", "Tab", "MetaLeft"];
+const keyboardKeys = getKeyboardKeys();
+
+const textarea = document.querySelector(".textarea");
+
+export function printVirtualSymbol(event) {
+  const currentKey = event.currentTarget.className.split(" ")[2];
+
+  if (keyboardKeys.includes(currentKey) && !options.includes(currentKey)) {
+    if (event.currentTarget.classList.contains("Space")) {
+      textarea.value += " ";
+      return;
+    }
+
+    textarea.value += event.target.innerText;
+  }
+}
+
+export function getKeyboardKeys() {
+  const activeKeys = document.querySelectorAll(".keyboard__key");
+  const activeButtons = Array.from(activeKeys).map((key) => key.className.split(" ")[2]);
+  return activeButtons;
+}
+
+export function printSymbol(event) {
+  const exclusion = {
+    ArrowUp: "▲",
+    ArrowDown: "▼",
+    ArrowLeft: "◄",
+    ArrowRight: "►",
+  };
+
+  if (keyboardKeys.includes(event.code) && !options.includes(event.code) && !Object.keys(exclusion).includes(event.code)) {
+    textarea.value += event.key;
+    return;
+  }
+
+  if (Object.keys(exclusion).includes(event.code)) {
+    textarea.value += exclusion[event.code];
+    return;
+  }
+
+  event.preventDefault();
+}
